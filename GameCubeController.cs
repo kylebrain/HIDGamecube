2bd5084dd6ae5ba3420fe978b189393024dd2cc6
Kyle-Laptop <kylebrain@gmail.com>
2018-11-15 16:43:28 -0800

    Back to directly imported library. Created GameCubeController class to provide an interface and cleaned main to show it off.

---
+++ GameCubeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using HidLibrary;
+
+namespace GameCubeHIDInterface
+{
+    public class GameCubeController
+    {
+        private const int VendorId = 0x0079;
+        private const int ProductId = 0x1844;
+
+        public uint ID
+        {
+            get
+            {
+                return controllerID;
+            }
+        }
+
+        private HidDevice device;
+        private uint controllerID;
+
+        public GameCubeController(uint id)
+        {
+            HidDevice[] devices = HidDevices.Enumerate(VendorId, ProductId).ToArray();
+            if(id > devices.Length || id == 0)
+            {
+                throw new System.ArgumentOutOfRangeException("Id is invalid!");
+            }
+            device = devices[id - 1];
+            if(device == null)
+            {
+                throw new System.Exception("Controller at this id is null!");
+            }
+            controllerID = id;
+
+        }
+
+        public void Rumble(float rumbleAmount)
+        {
+            if (rumbleAmount < 0 || rumbleAmount > 1)
+            {
+                throw new System.ArgumentOutOfRangeException("Amount must be between 0 and 1!");
+            }
+
+            int max = 0xF0;
+            int min = 0x60;
+
+            byte value;
+
+            if (rumbleAmount == 0)
+            {
+                value = 0;
+            }
+            else
+            {
+                value = (byte)((max - min) * rumbleAmount + min);
+            }
+
+            device.Write(new byte[2] {
+                0x00, value
+            });
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using GameCubeHIDInterface;
 using HidLibrary;
 
 namespace GameCubeHIDInterface
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-
+            GameCubeController controller1 = new GameCubeController(1);
+            GameCubeController controller2 = new GameCubeController(2);
+            controller1.Rumble(1);
+            System.Threading.Thread.Sleep(5000);
+            controller1.Rumble(0);
+            controller2.Rumble(1);
+            System.Threading.Thread.Sleep(5000);
+            controller2.Rumble(0);
         }
+
     }
 }
